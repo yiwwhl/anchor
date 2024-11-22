0676bc6de9531c0a230e4873ac34533247c12f56
yiwwhl <yiwwhl@qq.com>
2024-11-22 18:10:57 +0800

    feat: update wezterm theme to duskfox

--- tag-wezterm/config/wezterm/config/index.lua
+++ tag-wezterm/config/wezterm/config/index.lua
@@ -4,7 +4,7 @@ local window = require("config.window")
 local utils = require("utils")
 
 return utils.merge_tables({
-	color_scheme = "Catppuccin Mocha",
+	color_scheme = "duskfox",
 	line_height = 1.45,
 	window_close_confirmation = "NeverPrompt",
 }, {
